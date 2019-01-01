276f03aa8664aad731d0ac1dfa980f4b8b094a99
miketang <michael.c.tang@gmail.com>
2019-01-01 13:51:42 -0500

    Import bootstrap theme and add navbar component

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
-import '../styles/App.css';
+import NavBar from './NavBar'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          
-          
-        </header>
+      <div>
+        {/* Add Login component on launch */}
+        {/* <Login /> */}
+
+        {/* Render content if user is logged in */}
+        {/* Content */}
+        {/* RSVP Form */}
+
+        <NavBar />
+        <p>COMING SOON</p>
       </div>
     );
   }
--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,15 +1,23 @@
 module.exports = {
   Query: {
-    guest: async (_, { id }, { dataSources }) => {
+    guest: async (_, {
+      id
+    }, {
+      dataSources
+    }) => {
       return dataSources.guestAPI.getGuestById(id);
       // return dataSources.guestAPI.getGuestByEmail(email);
     },
   },
   Mutation: {
-    updateGuest: async (_, { id, rsvp }, { dataSources }) => {
+    updateGuest: async (_, {
+      id,
+      rsvp
+    }, {
+      dataSources
+    }) => {
       await dataSources.guestAPI.updateGuestById(id, rsvp);
       return dataSources.guestAPI.getGuestById(id);
     }
   }
-}; 
-
+};
